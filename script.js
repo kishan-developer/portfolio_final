6bb38462ab5c0d0573c39e4e0525ba2898f2a16b
kishan-developer <139308962+kishan-developer@users.noreply.github.com>
2023-07-22 16:54:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// alert('hii')
+
+// let button = document.getElementsByClassName('button_btn');
+
+// button.addEventListener("click",() => {
+//       const span = document.getElementsByClassName("button_btn");
+
+//       setTimeout( () => {
+
+//       },1000);
+// })
+
+
+const downloadbtn = document.querySelector('button_btn')
